5b1e4de70090caa5a7f824bbf08ee0ae47cdc37c
xosc <maxymcculloch@gmail.com>
2025-04-27 18:16:28 +1200

    add can_mine_block function

--- lickbot-plugins/src/utils/mining.rs
+++ lickbot-plugins/src/utils/mining.rs
@@ -1,10 +1,13 @@
 use azalea::auto_tool::best_tool_in_hotbar_for_block;
+use azalea::interact::pick;
 use azalea::inventory::SetSelectedHotbarSlotEvent;
-use azalea::{BlockPos, BotClientExt, Client};
+use azalea::world::ChunkStorage;
+use azalea::{BlockPos, BotClientExt, Client, Vec3, direction_looking_at};
 use tracing::warn;
 
 pub trait MiningExtrasClientExt {
     fn mine_with_best_tool(&self, pos: BlockPos) -> impl Future<Output = ()> + Send;
+    fn look_and_mine(&self, pos: BlockPos) -> impl Future<Output = ()> + Send;
 }
 
 impl MiningExtrasClientExt for Client {
@@ -29,7 +32,22 @@ impl MiningExtrasClientExt for Client {
             slot: best_tool_result.index as u8,
         });
 
+        self.look_and_mine(pos).await;
+    }
+
+    async fn look_and_mine(&self, pos: BlockPos) {
+        if !can_mine_block(pos, self.eye_position(), &self.world().read().chunks) {
+            warn!("Block is not reachable, not mining");
+            return;
+        }
+
         self.look_at(pos.center());
         self.mine(pos).await;
     }
+}
+
+pub fn can_mine_block(pos: BlockPos, eye_pos: Vec3, chunks: &ChunkStorage) -> bool {
+    let look_direction = direction_looking_at(&eye_pos, &pos.center());
+    let block_hit_result = pick(&look_direction, &eye_pos, chunks, 3.2);
+    block_hit_result.block_pos == pos
 }
